5c63c7ddb3d8e47f6531cd6b4dac52be7b634bf9
FelixOz13 <94942094+FelixOz13@users.noreply.github.com>
2022-11-30 23:39:57 -0600

    description commit

--- src/components/Flipbox.js
+++ src/components/Flipbox.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Flipbox() {
   return (
     <div>
       <div className="navbar">
-        <img className="navbar-logo" src="../images/mobileregistered.png" alt="" />
-        
+        <Link to="/intro">
+        <img className="navbar-logo" src="../images/mobileregistered.png" alt="">
+       </img></Link>
       <div className="flip-box">
         <div className="flip-box-inner">
           
@@ -16,8 +17,10 @@ function Flipbox() {
 
           <div class="flip-box-back">
       <img className="flag-logo4" src="../images/aguiladorada.jpeg" alt="Mexico"/>
+              </div>
+              
             </div>
-          </div>
+            
         </div>
       </div>
       </div>
