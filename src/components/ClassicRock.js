0aa1c6dedd3ff2969eb65f80b47e51250b20b433
FelixOz13 <94942094+FelixOz13@users.noreply.github.com>
2022-12-01 14:36:09 -0600

    description commit

--- src/components/ClassicRock.js
+++ src/components/ClassicRock.js
@@ -25,7 +25,7 @@ function ClassicRock() {
           <img className="indianaut2" src="./images/indianaut.png" alt=""/>
       <ButtonTreeCategories />
           <h1 className="slidertext">◄◄◄◄Desliza para Navegar ►►►►</h1>
-          <img className="indianaut1" src="./images/indianaut.png" alt=""/>
+          <img className="indianaut" src="./images/indianaut.png" alt=""/>
             <section className="cards-list">
                {cards6}
             </section>
