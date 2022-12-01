fd12952e4d46c612037b66ed2e4e5f40b34240df
FelixOz13 <94942094+FelixOz13@users.noreply.github.com>
2022-12-01 10:11:00 -0600

    description commit

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -25,8 +25,9 @@ function Intro() {
       <div className="video3">
         <video className="player-wrapper3"
           autoPlay ={true}
-          muted ={true}
-          loop ={true}
+          defaultMuted ={true}
+          loop={true}
+          playsInline={true}
           
           width="100%"
           height="100%"
